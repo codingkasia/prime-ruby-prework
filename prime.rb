673f326052d38c403f1ed03883c1a11e7deee160
KATARZYNA ROSENBERG <codingkasia@gmail.com>
2018-03-31 20:15:57 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+  answer = ""
+  start = 2 
+  range = (start...num)  
+  range.each do |n|
+    if(num% n ==0)
+      return false
+    end
+  end
+  return true 
+end
